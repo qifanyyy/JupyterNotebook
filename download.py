fe26dd58663d6e72c97233ed317369051096ad15
chenqihong <chenqh@uci.edu>
2020-06-15 03:40:06 -0700

    inperfect version (limit 1000 repos)

--- download.py
+++ download.py
@@ -14,6 +14,8 @@
 # Libraries #
 #############
 
+from shutil import copyfile
+import shutil
 import wget
 import time
 import simplejson
@@ -22,7 +24,11 @@ import pycurl
 import math
 from io import BytesIO
 import io
-
+import glob
+import os
+import zipfile
+dir_name = "/Users/qihongchen/desktop/notebooks/"
+extension = ".zip"
 
 
 #############
@@ -30,12 +36,14 @@ import io
 #############
 
 URL = "https://api.github.com/search/repositories?q=" #The basic URL to use the GitHub API
-QUERY = "jupyter+notebook" #The personalized query (for instance, to get repositories from user 'rsain')
-SUBQUERIES = [] #Different subqueries if you need to collect more than 1000 elements
+# The personalized query (for instance, to get repositories from user 'rsain')
+QUERY = "jupyternotebook&&extension:ipynb"
+# Different subqueries if you need to collect more than 1000 elements
+SUBQUERIES = ["+created%3A>%3D2020-01-1", "+created%3A<%3D2020-03-15"]
 PARAMETERS = "&per_page=100" #Additional parameters for the query (by default 100 items per page)
 DELAY_BETWEEN_QUERYS = 11 #The time to wait between different queries to GitHub (to avoid be banned)
-OUTPUT_FOLDER = "/Users/yuqifan/Documents/GitHub/JupyterNotebook" #Folder where ZIP files will be stored
-OUTPUT_CSV_FILE = "/Users/yuqifan/Documents/GitHub/JupyterNotebook/repositories.csv" #Path to the CSV file generated as output
+OUTPUT_FOLDER = "/Users/qihongchen/Desktop/notebooks/" #Folder where ZIP files will be stored
+OUTPUT_CSV_FILE = "/Users/qihongchen/Desktop/JupyterNotebook/repositories.csv" #Path to the CSV file generated as output
 
 
 #############
@@ -69,16 +77,18 @@ repositories = csv.writer(csvfile, delimiter=',')
 #Run queries to get information in json format and download ZIP file for each repository
 for subquery in range(1, len(SUBQUERIES)+1):
     print( "Processing subquery " + str(subquery) + " of " + str(len(SUBQUERIES)) + " ...")
-
     #Obtain the number of pages for the current subquery (by default each page contains 100 items)
     url = URL + QUERY + str(SUBQUERIES[subquery-1]) + PARAMETERS
+    # print("url = ", url)
     dataRead = simplejson.loads(getUrl(url))
+    # print("dataread = ", dataRead)
+    # print("dataRead.get('total_count') = ", dataRead.get('total_count'))
     numberOfPages = int(math.ceil(dataRead.get('total_count')/100.0))
-
     #Results are in different pages
     for currentPage in range(1, numberOfPages+1):
         print( "Processing page " + str(currentPage) + " of " + str(numberOfPages) + " ...")
         url = URL + QUERY + str(SUBQUERIES[subquery-1]) + PARAMETERS + "&page=" + str(currentPage)
+        print("url = ", url)
         dataRead = simplejson.loads(getUrl(url))
 
         #Iteration over all the repositories in the current json content page
@@ -105,3 +115,22 @@ for subquery in range(1, len(SUBQUERIES)+1):
 
 print ("DONE! " + str(countOfRepositories) + " repositories have been processed.")
 csvfile.close()
+
+os.chdir(dir_name)  # change directory from working dir to dir with files
+
+for item in os.listdir(dir_name):  # loop through items in dir
+    if item.endswith(extension):  # check for ".zip" extension
+        file_name = os.path.abspath(item)  # get full path of files
+        zip_ref = zipfile.ZipFile(file_name)  # create zipfile object
+        zip_ref.extractall(dir_name)  # extract file to dir
+        zip_ref.close()  # close file
+        os.remove(file_name)  # delete zipped file
+
+print("Sucessfully unzipped!")
+
+for file in glob.glob("**/*.ipynb", recursive=True):
+    copyfile(file, "/Users/qihongchen/desktop/notebooks/"+file.split('/')[-1])
+
+for file in os.listdir():
+    if os.path.isdir(file):
+        shutil.rmtree(file)
