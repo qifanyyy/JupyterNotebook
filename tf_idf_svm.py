accc7cd1804100ba021b4caf89914264bb3e6d20
chenqihong <chenqh@uci.edu>
2020-11-16 17:29:22 -0800

    Update tf_idf_svm.py

--- tf_idf_svm.py
+++ tf_idf_svm.py
@@ -8,9 +8,7 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 
-# Load data using Panda
 dataframe = open("result.txt")
-# print("training instances = ", dataframe.describe())
 # Split into a Training and Test Data
 x = dataframe["Implementation"]
 y = dataframe["Label"]
@@ -20,13 +18,9 @@ x_test, y_test = x[8860: ], y[8860:]
 cv = CountVectorizer()
 features = cv.fit_transform(x_train)
 # Build a model
-# tuned_parameters = {'kernel':['linear','rbf'], 'gamma':[1e-3, 1e-4], 'C':[1,10,100,1000]}
-# tuned_parameters = {'C': 1, 'gamma': 0.0001, 'kernel': ['rbf']}
-# model = GridSearchCV(svm.SVC(), tuned_parameters)
 model = svm.SVC(C=1, gamma= 0.0001, kernel = 'rbf')
 model.fit(features, y_train)
-#print("best parameters = ", model.best_params_)
-# Test accuracy
+
 features_test = cv.transform(x_test)
 
 print("Accuracy of the model is: ", model.score(features_test, y_test))
